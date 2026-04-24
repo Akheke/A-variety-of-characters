cb556c1f16526eab5dd1b6362c778e5f17dc5426
Akheke <Akheke105810@gmail.com>
2026-04-24 16:02:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function writeDisplay(input) {
           break;
         case 3:
           row = row + "@";
+          row = Math.random();
           break;
         default:
           row = row + "#";
